f342d61413c2adf6e75d3d1ae37884ff69f4069c
szywat <szywat04@gmail.com>
2025-05-15 15:54:45 +0200

    player "general" concept

---
+++ GryWojenne/src/General.java
@@ -0,0 +1,11 @@
+public class General {
+    private final String name;
+//    żołnierze
+//    monety
+
+    public General(String name) {
+        this.name = name;
+//        this.army = army
+//        this.money = money
+    }
+}
