7068086ff557068c871d22a52bc6b40071faa88c
szywat <szywat04@gmail.com>
2025-05-29 20:45:06 +0200

    rank names included

--- GryWojenne/src/Rank.java
+++ GryWojenne/src/Rank.java
@@ -1,5 +1,5 @@
 public class Rank {
-    private final int value;
+    private int value;
     public Rank(int value) {
         if (value < 1 || value > 4) {
             throw new IllegalArgumentException("Rank musi być w zakresie 1-4.");
@@ -7,13 +7,23 @@ public class Rank {
         this.value = value;
     }
 
-    public int getValue() {return value;}
-    public String getRank() {
-        if (value == 1) return "Szeregowy ( "+ value +" stopień )";
-        if (value == 2) return "Kapral ( "+ value +" stopień )";
-        if (value == 3) return "Kapitan ( "+ value +" stopień )";
-        if (value == 4) return "Major ( "+ value +" stopień )";
-        throw new IllegalArgumentException("Stopień powinien znajdować się w zakresie 1-4!");
-    }
+    public int getRank() {return value;}
+    public void addValue() {this.value += 1;}
+    public String getName() {
+        if (value == 1) {
+            return "Szeregowy";
+        }
+        if (value == 2) {
+            return "Kapral";
+        }
+        if (value == 3) {
+            return "Kapitan";
+        }
+        if (value == 4) {
+            return "Major";
+        } else {
+            throw new Error("Nieznana ranga");
+        }
+    };
 
 }
