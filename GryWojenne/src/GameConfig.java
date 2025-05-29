f8ed50587831215b88b702c32e150cbeaa80c5fc
szywat <szywat04@gmail.com>
2025-05-29 20:42:27 +0200

    game config

---
+++ GryWojenne/src/GameConfig.java
@@ -0,0 +1,6 @@
+public class GameConfig {
+    public static final int MONEY_LIMIT = 100;
+    public static final int WINNING = 3; // ilu majorów potrzeba ż
+    public static final int STARTING_ARMY = 3;
+
+}
