8ef8f5a368a197ffd4888b17aa1386a58231ccd7
szywat <szywat04@gmail.com>
2025-05-29 20:43:02 +0200

    general system

--- GryWojenne/src/General.java
+++ GryWojenne/src/General.java
@@ -1,11 +1,17 @@
 public class General {
     private final String name;
-//    żołnierze
-//    monety
+    private Army army;
+    private MoneyBag moneyBag;
 
-    public General(String name) {
+    public General(String name, int startingBalance) {
         this.name = name;
-//        this.army = army
-//        this.money = money
+        this.army = new Army();
+        this.moneyBag = new MoneyBag(startingBalance);
     }
+
+    public String getName() {return name;}
+    public Army getArmy() {return army;}
+    public int getMoney() {return moneyBag.getBalance();}
+    public boolean spendMoney(int amount) {return moneyBag.spendMoney(amount);}
+    public void addMoney(int amount) {moneyBag.addMoney(amount);}
 }
