0aff5f32ffbf3f19449f4a3664f2272793f86913
szywat <szywat04@gmail.com>
2025-05-29 20:46:12 +0200

    soldier exp developed

--- GryWojenne/src/Soldier.java
+++ GryWojenne/src/Soldier.java
@@ -1,26 +1,39 @@
-public class Soldier {
-    private Rank rank;
-    private int exp;
-    private int strength;
-    private boolean alive;
+public class Soldier extends Rank {
+    public Rank rank;
+    public int experience;
+    public int strength;
+    public int cost;
 
-    private Soldier(Rank rank, int exp) {
-        if (exp < 0) throw new IllegalArgumentException("Doświadczenie nie może być ujemne");
-        this.rank = rank;
-        this.exp = exp;
-        this.alive = exp > 0;
+
+    public Soldier(int rankValue, int experience) {
+        super(rankValue);
+        if (experience < 0) throw new IllegalArgumentException("Doświadczenie nie może być ujemne");
+        this.rank = new Rank(rankValue);
+        this.experience = experience;
+        this.getStrength();
+        this.cost = rankValue * 10;
     }
 
-    public Rank getRank() {return rank;}
-    public int getExp() {return exp;}
-    public int getStrength() {return rank.getValue() * exp;}
-    public boolean getAlive() {return alive;}
+    public int getRank() {return rank.getRank();}
+    public int getExp() {return experience;}
+    public int getStrength() {return this.strength = rank.getRank() * experience;}
+    public boolean isAlive() {return experience > 0;}
     public void addExp(int n) {
-        if (!alive) throw new IllegalArgumentException("Ten żołnierz nie żyje!");
-        if (n < 0) throw new IllegalArgumentException("Nie można odejmować doświadczenia!");
-        this.exp += n;
-    };
+        if (n < 0) throw new IllegalArgumentException("Podaj wartość dodatnią!");
 
+        this.experience += n;
+        if (this.experience > 5) {
+            this.experience = 5;
+        }
+        if (this.experience == 5 && this.rank.getRank() != 4) {
+            this.rank.addValue();
+            this.experience = 1;
+            System.out.println("Żołnierz awansuje! Jego nowa ranga: " + this.rank.getName());
+        }
 
-
+    }
+    public void subExp(int n) {
+        if (n < 0) throw new IllegalArgumentException("Podaj wartość dodatnią!");
+        this.experience -= n;
+    }
 }
